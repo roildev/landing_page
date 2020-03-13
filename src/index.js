e4c2c789c13b33a2b2aadf33bb65c8b2bc009f78
Roman Chagovtsev <4agovtsev@gmail.com>
2020-03-13 20:08:52 +0100

    Вторая модалка и начало работы с аккардионом

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import modalWindow1 from './modules/modalWindow1';
 import sendForms from './modules/sendForms';
+import accordion from './modules/accordion';
+import modalWindow2 from './modules/modalWindow2';
 
 
 modalWindow1();
 sendForms();
+accordion();
+modalWindow2();
---
+++ src/modules/modalWindow2.js
@@ -0,0 +1,27 @@
+const modalWindow2 = () => {
+ 
+     const discountBtn = document.querySelectorAll('.discount-btn'),
+     popupDiscount = document.querySelector('.popup-discount'),
+     popup = document.querySelectorAll('.popup');
+    
+     for(let i = 0; i < discountBtn.length; i++){
+        discountBtn[i].addEventListener('click', () => {
+            popupDiscount.style.display = 'block';
+        });
+     }
+     
+     popup[1].addEventListener('click', (event) => {
+        let target = event.target;
+        console.log('target: ', target);
+
+        if(target.classList.contains('popup-close') || 
+            target.classList.contains('popup')){
+            popupDiscount.style.display = 'none';
+        } 
+        
+    });
+
+
+};
+
+export default modalWindow2;
