6e888e3055be03852cc820fbe2c7a818786346d2
Roman Chagovtsev <4agovtsev@gmail.com>
2020-03-14 09:36:48 +0100

    Аккордион 2 готов

--- src/modules/accordion.js
+++ src/modules/accordion.js
@@ -1,48 +1,22 @@
 const accordion = () => {
- 
-    // const panelCollapse = document.querySelectorAll('.panel-collapse'),
-    //     collapseOne = document.getElementById('collapseOne-two'),
-    //     collapseTwo = document.getElementById('collapseTwo-two'),
-    //     collapseThree = document.getElementById('collapseThree-two'),
-    //     collapsed = document.querySelectorAll('.collapsed');
-        
 
-    //     collapsed.forEach((elem) => {            
-
-    //         if(elem.getAttribute('data-parent') === "#accordion-two"){
-    //             elem.addEventListener('click', () => {
-    //                 event.preventDefault();
-    //                 let target = event.target;
-    //                 console.log('target: ', target);
-
-    //                 // collapseOne.classList.toggle('in');
-    //                 // collapseTwo.classList.toggle('in');
-    //                 // collapseThree.classList.toggle('in');
-                    
-    //             });              
-    //         }            
-    //     });  
-    //};
+    const accordeonTwo = document.querySelector('#accordion-two'),
+    panelHeading = accordeonTwo.querySelectorAll('.panel-heading'),
+    panelCollapse = accordeonTwo.querySelectorAll('.panel-collapse');
     
-    const accordeonTwoParent = document.querySelector('#accordion-two'),
-        collapseOne = document.getElementById('collapseOne-two'),
-        collapseTwo = document.getElementById('collapseTwo-two'),
-        panelHeading = accordeonTwoParent.querySelectorAll('.panel-heading'),
-        panelContent = document.querySelectorAll('.collapse');
-        console.log('panelContent: ', panelContent[1]);
+    panelHeading.forEach((elem) => {
+        elem.addEventListener('click', (event) => {
+            let target = event.target.closest('.panel');
 
-        panelHeading.forEach((elem) => {
-            elem.addEventListener('click', (event) => {
-                event.preventDefault();
-                let target = event.target;
-                
-                if (target.closest('.panel-heading')) {
-                    console.log('ok')
-                    collapseOne.classList.toggle('in');
-                }
-            });
-        });
-  
+            panelCollapse.forEach((section) => {
+                if(target.contains(section)){                    
+                    section.classList.add('in');
+                } else {
+                    section.classList.remove('in');
+                }                
+            }); 
+        });    
+    });  
 };
 
 export default accordion;
